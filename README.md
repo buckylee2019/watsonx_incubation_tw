d4abe19fe909c0c8af0d707ba7a0cf5636f0aa12
buckylee <little880536@gmail.com>
2023-11-14 17:56:20 +0800

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# GenAI_Incubation_watsonx_中文（简体）
+# GenAI_Incubation_watsonx 中文
 
 Github for IBM Watsonx Incubation Program. 
 
 
 <img width="1251" alt="Screenshot 2023-10-23 at 5 46 47 AM" src="https://github.com/xysong1201/Incubation_watsonx_Chinese/blob/main/images/agenda.png">
+# wx-esg-report
--- lab06a_Building Question-Answering with watsonx.ai and Streamlit with Retrieval Augmented Generation (Transient)/langChainInterface.py
+++ lab06a_Building Question-Answering with watsonx.ai and Streamlit with Retrieval Augmented Generation (Transient)/langChainInterface.py
@@ -1,37 +1,28 @@
 
-from typing import Any, List, Mapping, Optional, Union, Dict
-from pydantic import BaseModel, Extra
+from typing import Any, List, Mapping, Optional,Dict
+
+from langchain.callbacks.manager import CallbackManagerForLLMRun
 from langchain.llms.base import LLM
 from ibm_watson_machine_learning.foundation_models import Model
 from langchain.llms.utils import enforce_stop_tokens
 
 # define LangChainInterface model
-class LangChainInterface(LLM, BaseModel):
-
+class LangChainInterface(LLM):
     credentials: Optional[Dict] = None
     model: Optional[str] = None
     params: Optional[Dict] = None
     project_id : Optional[str]=None
-
-    class Config:
-        """Configuration for this pydantic object."""
-        extra = Extra.forbid
-
-    @property
-    def _identifying_params(self) -> Mapping[str, Any]:
-        """Get the identifying parameters."""
-        _params = self.params or {}
-        return {
-            **{"model": self.model},
-            **{"params": _params},
-        }
-    
     @property
     def _llm_type(self) -> str:
-        """Return type of llm."""
         return "IBM WATSONX"
 
-    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+    def _call(
+        self,
+        prompt: str,
+        stop: Optional[List[str]] = None,
+        run_manager: Optional[CallbackManagerForLLMRun] = None,
+        **kwargs: Any,
+    ) -> str:
         """Call the WatsonX model"""
         params = self.params or {}
         model = Model(model_id=self.model, params=params, credentials=self.credentials, project_id=self.project_id)
@@ -39,3 +30,12 @@ class LangChainInterface(LLM, BaseModel):
         if stop is not None:
             text = enforce_stop_tokens(text, stop)
         return text
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        
+        _params = self.params or {}
+        return {
+            **{"model": self.model},
+            **{"params": _params},
+        }
